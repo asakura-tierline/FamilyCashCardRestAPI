80b99cc3f4fc97ae3c893b58699b905b99c856c4
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-20 14:32:10 +0900

    Add CashCardJsonTest class with an initial failing test to demonstrate Test-First development methodology

---
+++ cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package example.cashcard;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class CashCardJsonTest {
+
+   @Test
+   void myFirstTest() {
+      assertThat(42).isEqualTo(42);
+   }
+}
