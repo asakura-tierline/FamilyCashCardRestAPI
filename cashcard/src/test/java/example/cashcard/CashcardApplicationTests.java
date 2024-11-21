7c33f9522d30ec21a7bb1f8a36ee7c0b4bacb244
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-21 11:11:56 +0900

    Refactor POST /cashcards test to validate semantic correctness: Check for 201 Created status, Location header with new resource URI, and successful retrieval of created CashCard

--- cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
+++ cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import java.net.URI;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
@@ -39,10 +40,15 @@ void shouldNotReturnACashCardWithAnUnknownId() {
   assertThat(response.getBody()).isBlank();
 }
 
+
 @Test
 void shouldCreateANewCashCard() {
    CashCard newCashCard = new CashCard(null, 250.00);
    ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
-   assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+   assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+   URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+   ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+   assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 }
 }
