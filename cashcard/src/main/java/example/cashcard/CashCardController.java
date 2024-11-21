5eab00ff64fadc48743bc48812c96ec5732e38ee
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-21 14:54:49 +0900

    Update CashCardController to enforce ownership: Modify GET endpoints to use Principal for filtering by owner, integrating findByIdAndOwner and findByOwner repository methods. Ensure authenticated users only access their own data.'

--- cashcard/src/main/java/example/cashcard/CashCardController.java
+++ cashcard/src/main/java/example/cashcard/CashCardController.java
@@ -1,6 +1,7 @@
 package example.cashcard;
 
 import java.net.URI;
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,8 +28,8 @@ class CashCardController {
     }
 
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
+        Optional<CashCard> cashCardOptional = Optional.ofNullable(cashCardRepository.findByIdAndOwner(requestedId, principal.getName()));
         if (cashCardOptional.isPresent()) {
             return ResponseEntity.ok(cashCardOptional.get());
         } else {
@@ -47,10 +48,10 @@ class CashCardController {
     }
 
     @GetMapping
-    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
-        Page<CashCard> page = cashCardRepository.findAll(
-                PageRequest.of(
-                        pageable.getPageNumber(),
+private ResponseEntity<List<CashCard>> findAll(Pageable pageable, Principal principal) {
+    Page<CashCard> page = cashCardRepository.findByOwner(principal.getName(),
+            PageRequest.of(
+                pageable.getPageNumber(),
                         pageable.getPageSize(),
                         pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 ));
