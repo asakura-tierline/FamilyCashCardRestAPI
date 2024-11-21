8104dfd15962f406b324d16f4b8856e104eb60c0
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-21 12:11:45 +0900

    Improve GET /cashcards test: Add assertions to validate JSON response length, ids, and amounts; fix test data expectation mismatch for CashCard amounts

--- cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
+++ cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,8 +1,8 @@
 package example.cashcard;
 
-import java.net.URI;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,10 +10,11 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
@@ -65,6 +66,17 @@ class CashCardApplicationTests {
  void shouldReturnAllCashCardsWhenListIsRequested() {
      ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
      assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+     DocumentContext documentContext = JsonPath.parse(response.getBody());
+     int cashCardCount = documentContext.read("$.length()");
+     assertThat(cashCardCount).isEqualTo(3);
+
+     JSONArray ids = documentContext.read("$..id");
+     assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
+
+     JSONArray amounts = documentContext.read("$..amount");
+     assertThat(amounts).containsExactlyInAnyOrder(123.45, 1.00, 150.00);
  }
+
 
 }
