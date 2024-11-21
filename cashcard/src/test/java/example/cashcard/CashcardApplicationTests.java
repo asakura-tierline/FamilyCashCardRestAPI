8d0b54e15a6110267c330e1dec540c6fbd9b520f
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-21 11:49:15 +0900

    Improve CashCard API tests: Add assertions for id and amount validation, update test data with multiple entries, and refactor serialization/deserialization logic

--- cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
+++ cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,19 +1,23 @@
 package example.cashcard;
 
-import java.net.URI;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
     @Autowired
     TestRestTemplate restTemplate;
@@ -21,7 +25,6 @@ class CashCardApplicationTests {
     @Test
     void shouldReturnACashCardWhenDataIsSaved() {
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
-
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         DocumentContext documentContext = JsonPath.parse(response.getBody());
@@ -30,34 +33,32 @@ class CashCardApplicationTests {
 
         Double amount = documentContext.read("$.amount");
         assertThat(amount).isEqualTo(123.45);
-
     }
 
     @Test
-void shouldNotReturnACashCardWithAnUnknownId() {
-  ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+    void shouldNotReturnACashCardWithAnUnknownId() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
 
-  assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-  assertThat(response.getBody()).isBlank();
-}
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+    }
 
+    @Test
+    //@DirtiesContext
+    void shouldCreateANewCashCard() {
+        CashCard newCashCard = new CashCard(null, 250.00);
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
-@Test
-void shouldCreateANewCashCard() {
-  CashCard newCashCard = new CashCard(44L, 250.00);
-   ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
-   assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-   URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
-   ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
-   assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        Number id = documentContext.read("$.id");
+        Double amount = documentContext.read("$.amount");
 
-   DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
-   Number id = documentContext.read("$.id");
-   Double amount = documentContext.read("$.amount");
-
-   assertThat(id).isNotNull();
-   assertThat(amount).isEqualTo(250.00);
-  }
-
+        assertThat(id).isNotNull();
+        assertThat(amount).isEqualTo(250.00);
+    }
 }
