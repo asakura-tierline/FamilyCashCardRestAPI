58c090492109c29fd5aaf4c8b6b38a68d75abc70
yuta asakura <yuta.asakura.se@gmail.com>
2024-11-21 11:25:03 +0900

    Add assertions to validate new CashCard id and amount in POST /cashcards tests; explore behavior when providing an id during creation and identify potential validation needs

--- cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
+++ cashcard/src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import java.net.URI;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
@@ -43,12 +44,20 @@ void shouldNotReturnACashCardWithAnUnknownId() {
 
 @Test
 void shouldCreateANewCashCard() {
-   CashCard newCashCard = new CashCard(null, 250.00);
+  CashCard newCashCard = new CashCard(44L, 250.00);
    ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
    assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
 
    URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
    ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
    assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-}
+
+   DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+   Number id = documentContext.read("$.id");
+   Double amount = documentContext.read("$.amount");
+
+   assertThat(id).isNotNull();
+   assertThat(amount).isEqualTo(250.00);
+  }
+
 }
